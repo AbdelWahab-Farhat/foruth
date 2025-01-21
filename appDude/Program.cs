0ce4f4188e275c28b5894c4a6ff295c8ed56d6ce
abdelwahab farhat <abdelwahab.dev@gmail.com>
2025-01-21 11:50:24 +0800

    commit Bro

--- appDude/Program.cs
+++ appDude/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Stack<int> myStack = new Stack<int>(10);
+myStack.Push(10);
+myStack.Push(20);
+myStack.Push(30);
+myStack.Push(40);
+myStack.Push(50);
+
+System.Console.WriteLine("The elements in the Stack are: " + myStack.Count);
+
+foreach (var item in myStack)
+{
+    Console.WriteLine(item);
+}
